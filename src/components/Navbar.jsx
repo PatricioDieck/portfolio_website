173eff4b8f298652485c495aa2174cf8d562bb08
Patricio Dieck <patricio.dieck@outlook.com>
2023-05-24 13:08:53 -0500

    intial_commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+
+import {styles} from '../styles'
+import { NavLinks } from '../constants'
+import {logo, menu, close} from '../assets'
+
+const Navbar = () => {
+  useState[active, setActive] = useState('')
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+
+      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+
+        <Link 
+          to='/'
+          className='flex items-center gap-2'
+          onClick = {() => {
+            setActive('');
+          }}/>
+
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
