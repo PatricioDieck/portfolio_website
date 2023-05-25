f928a305cef6441a51a6975bc3b8efc190248d90
Patricio Dieck <patricio.dieck@outlook.com>
2023-05-24 22:49:30 -0500

    not working rn

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react'
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls, Preload, useGlTF} from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  //pass in the computer scene (path to gltf file)
+  const computer = useGlTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight 
+      intensity={0.15}
+      groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive 
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.5, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+         />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{position: [20, 3, 5], fov: 25}}
+      gl={{preserveDrawingBuffer: true}}
+      >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}/>
+        <Computers />
+
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+;}
+
+export default ComputersCanvas
