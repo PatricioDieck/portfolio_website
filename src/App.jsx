436746aeea02dfb8145ec550579aa400c448b977
Patricio Dieck <patricio.dieck@outlook.com>
2023-05-31 16:01:45 -0500

    added and took out the stars

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         {/* <Feedbacks /> */}
         <div className= 'relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react"
 import { motion } from "framer-motion"
-import emailjs from '@emailjs/browser'
+import { MailIcon, PhoneIcon, LocationMarkerIcon } from '@heroicons/react/outline' // Ensure to install @heroicons package
+
 
 import { styles } from "../styles"
 import { EarthCanvas } from "./canvas"
@@ -8,80 +9,41 @@ import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motion"
 
 const Contact = () => {
-  const formRef = useRef()
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  })
-  const [loading, setLoading] = useState(false)
-
-  const handleChange = (e) => {}
-
-  const handleSubmit = (e) => {}
 
   return (
     <div
-      className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"
+      className="xl:mt-8 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden "
     >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-        >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact</h3>
+    <motion.div
+      variants={slideIn("left", "tween", 0.2, 1)}
+      className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+    >
+      <p className={styles.sectionSubText}>Get in touch</p>
+      <h3 className={styles.sectionHeadText}>Contact</h3>
+      <div className="space-y-10 mb-10 mt-8 text-lg">
+        <div className="flex items-center mt-4">
+          <MailIcon className="h-6 w-6 mr-2 text-white" />
+          <a href="mailto:pdieck98@gmail.com" className="text-white fontsize-lg hover:text-green-500">pdieck98@gmail.com</a>
+        </div>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-2'
-          >
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Name</span>
-              <input 
-                type="text"
-                name="name"
-                value={form.name}
-                onChange={handleChange}
-                placeholder="What's Your Name?"
-                className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg text-white outline-none border-none font-medium"
-              />
-            </label>
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Email</span>
-              <input 
-                type="email"
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-                placeholder="What's Your Email?"
-                className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg text-white outline-none border-none font-medium"
-              />
-            </label>
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Message</span>
-              <textarea 
-                rows='7'
-                name="message"
-                value={form.message}
-                onChange={handleChange}
-                placeholder="What do you want to say?"
-                className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg text-white outline-none border-none font-medium"
-              />
-            </label>
+        <div className="flex items-center mt-4">
+          <PhoneIcon className="h-6 w-6 mr-2 text-white" />
+          <a href="tel:+15125902601" className="text-white fontsize-lg hover:text-green-500">(512) 590-2601</a>
+        </div>
 
-            <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
-            >
-              {loading ? "Sending..." : "Send"}
-            </button>
-          </form>
-      </motion.div>
+        <div className="flex items-start mt-4">
+          <LocationMarkerIcon className="h-6 w-6 mr-2 text-white" />
+          <div className="text-white fontsize-lg">
+            <p>301 E 47th St. ∙ New York, New York</p>
+            <p className="mt-2">17912 Clairess Ln ∙ Manor, TX</p>
+          </div>
+        </div>
+      </div>
+    </motion.div>
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[500px] h-[350px]"
+        className="xl:flex-1 xl:h-auto md:h-[500px] h-[550px]"
       >
         <EarthCanvas />
       </motion.div>
