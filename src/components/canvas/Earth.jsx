1f8f3557899e4e49f462843a9fd748f251f654d6
Patricio Dieck <patricio.dieck@outlook.com>
2023-05-31 08:15:17 -0500

    earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,10 +6,35 @@ import CanvasLoader from "../Loader"
 
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf")
+  const earth = useGLTF("./manhattan/scene.gltf")
   
   return (
-    <div>Earth</div>
+    <mesh>
+      <hemisphereLight 
+        skyColor={'#FFDAB9'} // peachy color
+        groundColor='orange' 
+        intensity={0.9}
+      />
+      <directionalLight
+        position={[-50, 75, 100]} // set the direction of the light
+        color={'#FFD700'} // golden color
+        intensity={0.5}
+        castShadow
+      />
+      <spotLight
+        position={[-20, 50, 90]}
+        angle={0.08}
+        penumbra={2}
+        intensity={0.5} 
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={earth.scene}
+        position={[0, 0, 0]}
+        camera={{fov: 45, near: 0.1, far: 200, position: [0, 43, 76]}}
+      />
+    </mesh>
   )
 }
 
@@ -24,7 +49,9 @@ const EarthCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls 
           autoRotate={true}
-          enableZoom={true}
+          autoRotateSpeed={0.2}
+          enableZoom={false}
+          minDistance={6.8} maxDistance={6.8}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2} 
         />
