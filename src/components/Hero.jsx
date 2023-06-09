9fb4278680b42bca1dac6ed86272bd0e3351bb61
Patricio Dieck <patricio.dieck@outlook.com>
2023-06-09 15:39:51 -0500

    no computer in beginnning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         </div>
       </div>
       
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
